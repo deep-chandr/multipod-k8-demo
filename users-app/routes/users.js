1d836172fa8d7bb308725ca6e63ec1c80fdcc807
Deep <deep.chandr@fashinza.com>
2024-04-28 02:16:42 +0530

    Created docker file & deployment file for users app

--- users-app/routes/users.js
+++ users-app/routes/users.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const myUsers = [{ username: "test", password: "test" }];
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get("/", function (req, res, next) {
+  res.json({ users: myUsers });
+});
+
+router.post("/", function (req, res, next) {
+  // Save locally
+  // than save it in auth service
+  const { username, password } = req.body;
+  myUsers.push({ username, password });
+  res.json({ username, password });
 });
 
 module.exports = router;
